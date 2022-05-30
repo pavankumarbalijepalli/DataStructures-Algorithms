9d02b1fa13ec6cd65f122c11110f3e2350b498eb
Pavan Kumar Balijepalli <54879527+pavankumarbalijepalli@users.noreply.github.com>
2022-05-30 23:12:07 +0530

    Add Longest Common Prefix solution

---
+++ Leet_Code_Problems/Longest_Common_Prefix.py
@@ -0,0 +1,15 @@
+# Question: https://leetcode.com/problems/longest-common-prefix/
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        prefix = strs[0]
+        strs = strs[1:]
+        while strs:
+            temp = ""
+            while (prefix and strs[0]) and (prefix[0]==strs[0][0]):
+                temp += prefix[0]
+                prefix = prefix[1:]
+                strs[0] = strs[0][1:]
+            prefix = temp
+            strs = strs[1:]
+        return prefix
